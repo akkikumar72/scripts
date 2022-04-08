a70a53b84260fcdf628563bc2ef342280bb62774
Brandon <brandon@pangolin.exchange>
2022-04-08 12:38:32 -0400

    Extra cached helpers

--- src/core/helpers.js
+++ src/core/helpers.js
@@ -4,11 +4,25 @@ const web3 = new Web3(new Web3.providers.HttpProvider('https://api.avax.network/
 
 const Helpers = {
     symbolCache: {},
+    token0Cache: {},
+    token1Cache: {},
 
     getSymbolCached: async (address) => {
         if (Helpers.symbolCache[address]) return Helpers.symbolCache[address];
         const contract = new web3.eth.Contract(ABI.TOKEN, address);
         return Helpers.symbolCache[address] = await contract.methods.symbol().call();
+    },
+
+    getToken0Cached: async (address) => {
+        if (Helpers.token0Cache[address]) return Helpers.token0Cache[address];
+        const contract = new web3.eth.Contract(ABI.PAIR, address);
+        return Helpers.token0Cache[address] = await contract.methods.token0().call();
+    },
+
+    getToken1Cached: async (address) => {
+        if (Helpers.token1Cache[address]) return Helpers.token1Cache[address];
+        const contract = new web3.eth.Contract(ABI.PAIR, address);
+        return Helpers.token1Cache[address] = await contract.methods.token1().call();
     },
 
     toChecksumAddress: (address) => web3.utils.toChecksumAddress(address.toLowerCase()),
